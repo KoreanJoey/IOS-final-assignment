c17eee9d81cf5b074b16f6c1491e2d281b461344
KoreanJoey <165756862+KoreanJoey@users.noreply.github.com>
2024-04-22 10:09:38 +1000

    Make application

---
+++ Application/Application/ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationApp.swift
+//  Application
+//
+//  Created by 조성하 on 22/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

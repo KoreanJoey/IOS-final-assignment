efdee570efc0dcee01038c0ab6ae285405fb8d67
Nocolororororo <robotsok@gmail.com>
2024-04-30 12:57:17 +1000

    New button

--- Application/Application/AddListView.swift
+++ Application/Application/AddListView.swift
@@ -17,13 +17,23 @@ struct AddListView: View {
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .padding()
+            Text("Food Name")
+                .font(.title)
             TextField("Product Name", text: $listModel.productName)
                         .frame(width: 200, height: 50)
                         .tint(.cyan)
                         .padding([.leading, .trailing], 10)
                         .textFieldStyle(.roundedBorder)
                         
-            Text("Hello")
+            Text("Expiry's on")
+            Button("Custom Date"){}
+            Button("Expire on 1 day"){}
+            Button("Expire on 4 days"){}
+            Button("Expire on 7 days"){}
+            
+            Text("Notify me on...")
+            Button("1 day before expire"){}
+            Button("4 days before expire"){}
             
         }
         .offset(y:-650)

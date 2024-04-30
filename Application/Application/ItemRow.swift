1db8ec15f58a7edf3b2f5b5c4e7c73abd01e4b2e
KoreanJoey <165756862+KoreanJoey@users.noreply.github.com>
2024-04-30 12:31:24 +1000

    Make ItemRow

---
+++ Application/Application/ItemRow.swift
@@ -0,0 +1,28 @@
+//
+//  ItemRow.swift
+//  Application
+//
+//  Created by 조성하 on 30/4/2024.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var item : ListItem
+    
+    var body: some View {
+        HStack {
+            item.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            VStack {
+                Text(item.name)
+                Text(item.expiredDate)
+            }
+        }
+    }
+}
+
+#Preview {
+    ItemRow()
+}

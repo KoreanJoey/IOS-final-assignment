823987e75a95cab9e53ff33dfd42e741629cb3a2
KoreanJoey <165756862+KoreanJoey@users.noreply.github.com>
2024-05-13 17:12:48 +1000

    make new function to store data into directory file

--- Application/Application/Model/ModelData.swift
+++ Application/Application/Model/ModelData.swift
@@ -53,3 +53,80 @@ func save(_ item : ListItem, filename: String) {
         print("Couldn't save new entry to \(filename), \(error.localizedDescription)")
     }
 }
+
+//copy the file
+func copyFileToDocumentsDirectory(filename: String) {
+    let fileManager = FileManager.default
+    
+    // 번들에서 파일 URL을 구합니다.
+    guard let bundleURL = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        print("JSON file is missing in the bundle.")
+        return
+    }
+    
+    // "Documents" 디렉토리의 경로를 구합니다.
+    guard let documentsDirectory = try? fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false) else {
+        print("Documents directory not found.")
+        return
+    }
+    
+    let destinationURL = documentsDirectory.appendingPathComponent("\(filename).json")
+    
+    // 파일이 이미 존재하는지 확인합니다.
+    if fileManager.fileExists(atPath: destinationURL.path) {
+        print("File already exists in Documents directory.")
+        return
+    }
+    
+    // 파일을 복사합니다.
+    do {
+        try fileManager.copyItem(at: bundleURL, to: destinationURL)
+        print("File copied successfully.")
+    } catch {
+        print("Failed to copy file: \(error)")
+    }
+}
+
+//load data from file
+func loadDataFromDocumentsDirectory(filename: String) -> Data? {
+    let fileManager = FileManager.default
+    
+    // "Documents" 디렉토리의 경로를 구합니다.
+    guard let documentsDirectory = try? fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false) else {
+        print("Documents directory not found.")
+        return nil
+    }
+    
+    let fileURL = documentsDirectory.appendingPathComponent("\(filename).json")
+    
+    // 파일에서 데이터를 로드합니다.
+    if let data = try? Data(contentsOf: fileURL) {
+        return data
+    } else {
+        print("Failed to load data from file.")
+        return nil
+    }
+}
+
+
+//save data to file
+func saveDataToDocumentsDirectory(data: Data, filename: String) {
+    let fileManager = FileManager.default
+    
+    // "Documents" 디렉토리의 경로를 구합니다.
+    guard let documentsDirectory = try? fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false) else {
+        print("Documents directory not found.")
+        return
+    }
+    
+    let fileURL = documentsDirectory.appendingPathComponent("\(filename).json")
+    
+    // 데이터를 파일에 씁니다.
+    do {
+        try data.write(to: fileURL, options: .atomic)
+        print("Data written to file successfully.")
+    } catch {
+        print("Failed to write data to file: \(error)")
+    }
+}
+

3a5b2b7aca740b54aa24ecfdcb9aa3fd746fb7fe
KoreanJoey <165756862+KoreanJoey@users.noreply.github.com>
2024-05-13 23:37:57 +1000

    add core data and recipe data & recipe manager

--- Application/Application/RecipeManager.swift
+++ Application/Application/RecipeManager.swift
@@ -8,5 +8,45 @@
 import Foundation
 
 class RecipeManager: ObservableObject {
+    @Published var recipeData: RecipeData?
+    
+    let recipieURL = "https://api.spoonacular.com/recipes/findByIngredients"
+    let APIKey = "3403d896f67348bdaa5252edb0285c81"
+    
+    
+    func SearchRecipiesByIngredients(ingredient: String){
+        let urlString = "\(recipieURL)?ingredients=\(ingredient)"
+        
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+            if let url = URL(string: urlString) {
+                let session = URLSession(configuration: .default)
+                let task = session.dataTask(with: url) { (data, response, error) in
+                    if let error = error {
+                        print(error)
+                        return
+                    }
+                    if let safeData = data {
+                        self.parseJSON(recipeData: safeData)
+                    }
+                }
+                task.resume()
+            }
+        }
+        
+        func parseJSON(recipeData: Data) {
+            let decoder = JSONDecoder()
+            do {
+                let decodedData = try decoder.decode(RecipeData.self, from: recipeData)
+                DispatchQueue.main.async {
+                    self.recipeData = decodedData
+                }
+            } catch {
+                print(error)
+            }
+        }
+    
     
 }

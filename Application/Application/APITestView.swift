4dc31d94ab706c5b361e145ff17b41ed649c47c4
KoreanJoey <165756862+KoreanJoey@users.noreply.github.com>
2024-05-16 23:05:25 +1000

    finish api part made recipeSearchView

--- Application/Application/APITestView.swift
+++ Application/Application/APITestView.swift
@@ -1,52 +1,52 @@
+////
+////  RecipeView.swift
+////  Application
+////
+////  Created by 조성하 on 16/5/2024.
+////
 //
-//  APITestView.swift
-//  Application
+//import SwiftUI
 //
-//  Created by 조성하 on 16/5/2024.
+//struct RecipeView: View {
+//    @State private var ingredientIHave = ""
+//    @State private var recipeTitle = ""
+//    @State private var recipeManager = RecipeManager()
+//    
+//    
+//    var body: some View {
+//        VStack{
+//            TextField("Enter ingredient", text: $ingredientIHave )
+//                .padding()
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//            Button("Search") {
+//                recipeManager.fetchRecipiesByIngredients(ingredient: ingredientIHave)
+//            }
+//            .padding()
+//            
+//            if recipeManager.noRecipesFound {
+//                Text("No recipes matched your ingredient. Please try another ingredient.")
+//                    .foregroundColor(.red)
+//                    .padding()
+//            } else {
+//                
+//            List(recipeManager.recipes, id: \.self) { recipe in
+//                HStack() {
+//                    URLImageView(urlString: "\(recipe.image)", width: 100, height: 100)
+//                    Text(recipe.title)
+//                        .fontWeight(.bold)
+//                        }
+//                    }
+//                }
+//        }
+//        .onReceive(recipeManager.$recipes) { recipes in
+//            if let firstRecipe = recipes.first {
+//                self.recipeTitle = firstRecipe.title
+//            }
+//        }
 //
-
-import SwiftUI
-
-struct APITestView: View {
-    @State private var ingredientIHave = ""
-    @State private var recipeTitle = ""
-    @State private var recipeManager = RecipeManager()
-    
-    
-    var body: some View {
-        VStack{
-            TextField("Enter ingredient", text: $ingredientIHave )
-                .padding()
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            Button("Search") {
-                recipeManager.fetchRecipiesByIngredients(ingredient: ingredientIHave)
-            }
-            .padding()
-            
-            if recipeManager.noRecipesFound {
-                Text("No recipes matched your ingredient. Please try another ingredient.")
-                    .foregroundColor(.red)
-                    .padding()
-            } else {
-                
-            List(recipeManager.recipes, id: \.self) { recipe in
-                HStack() {
-                    URLImageView(urlString: "\(recipe.image)", width: 100, height: 100)
-                    Text(recipe.title)
-                        .fontWeight(.bold)
-                        }
-                    }
-                }
-        }
-        .onReceive(recipeManager.$recipes) { recipes in
-            if let firstRecipe = recipes.first {
-                self.recipeTitle = firstRecipe.title
-            }
-        }
-
-    }
-}
-
-#Preview {
-    APITestView()
-}
+//    }
+//}
+//
+//#Preview {
+//    RecipeView()
+//}
